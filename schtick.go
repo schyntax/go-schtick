350540438787c15be73bbbccd9ee5c7a4a1e46a2
Bret Copeland <bret@atlantisflight.org>
2015-09-28 02:12:30 -0400

    Work in progress. Nothing works.

--- schtick.go
+++ schtick.go
@@ -1,1 +1,106 @@
 package schtick
+
+import (
+	"errors"
+	"github.com/satori/go.uuid"
+	"github.com/schyntax/go-schyntax"
+	"sync"
+	"time"
+)
+
+type Options struct {
+	Name           string
+	ScheduleString string
+	Schedule       schyntax.Schedule
+	Window         time.Duration
+	LastKnownEvent time.Time
+	DisableAutoRun bool
+}
+
+type Schtick interface {
+	AddTask(options Options) Task
+	GetTaskByName(name string) Task
+	RemoveTask(name string) bool
+	IsShuttingDown() bool
+	Shutdown()
+}
+
+var _ Schtick = &schtickImpl{}
+
+type schtickImpl struct {
+	tasksLock      sync.RWMutex
+	tasks          map[string]*taskImpl
+	heapLock       sync.RWMutex
+	heap           pendingEventHeap
+	isShuttingDown bool
+}
+
+func New() Schtick {
+	// todo
+}
+
+func (s *schtickImpl) AddTask(options Options) (Task, error) {
+	hasStr := options.ScheduleString != ""
+	hasObj := options.Schedule != nil
+
+	if hasStr == hasObj {
+		return nil, errors.New("Schtick.AddTask: Must provide either ScheduleString or Schedule, but not both.")
+	}
+
+	sch := options.Schedule
+	if hasStr {
+		var err error
+		sch, err = schyntax.New(options.ScheduleString)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	name := options.Name
+	if name == "" {
+		name = uuid.NewV4().String()
+	}
+
+	s.tasksLock.Lock()
+	defer s.tasksLock.Unlock()
+
+	if s.tasks[name] != nil {
+		return nil, errors.New(`A scheduled task named "` + name + `" already exists.`)
+	}
+
+	task := taskImpl{}
+	task.schtick = s
+	task.name = name
+	task.schedule = sch
+	task.window = options.Window
+	task.isAttached = true
+
+	// add
+
+	// auto run
+
+	return task
+}
+
+func (s *schtickImpl) GetTaskByName(name string) Task {
+	s.tasksLock.RLock()
+	defer s.tasksLock.RUnlock()
+
+	return s.tasks[name]
+}
+
+func (s *schtickImpl) RemoveTask(name string) bool {
+	// todo
+}
+
+func (s *schtickImpl) IsShuttingDown() bool {
+	return s.isShuttingDown
+}
+
+func (s *schtickImpl) Shutdown() {
+	// todo
+}
+
+func (s *schtickImpl) poll() {
+	// todo
+}
--- task.go
+++ task.go
@@ -2,21 +2,13 @@ package schtick
 
 import (
 	"github.com/schyntax/go-schyntax"
+	"sync"
 	"time"
 )
 
-type Options struct {
-	Name string
-	ScheduleString string
-	Schedule schyntax.Schedule
-	Window time.Duration
-	LastKnownEvent time.Time
-	DisableAutoRun bool
-}
-
 type Task interface {
 	Name() string
-	Schedule() schyntax.Schedule
+	Schedule() *schyntax.Schedule
 	IsScheduleRunning() bool
 	IsAttached() bool
 	Window() time.Duration
@@ -25,14 +17,84 @@ type Task interface {
 	PrevEvent() time.Time
 	StartSchedule()
 	StopSchedule()
-	UpdateScheduleString(schedule string)
-	UpdateSchedule(schedule schyntax.Schedule)
+	UpdateScheduleString(schedule string) error
+	UpdateSchedule(schedule *schyntax.Schedule)
 }
 
+var _ Task = &taskImpl{}
+
 type taskImpl struct {
-	//
+	scheduleLock      sync.Mutex
+	runId             int
+	execLocked        int32
+	schtick           *schtickImpl
+	name              string
+	schedule          schyntax.Schedule
+	isScheduleRunning bool
+	isAttached        bool
+	window            time.Duration
+	nextEvent         time.Time
+	prevEvent         time.Time
+}
+
+func (t *taskImpl) Name() string {
+	return t.name
+}
+
+func (t *taskImpl) Schedule() schyntax.Schedule {
+	return t.schedule
+}
+
+func (t *taskImpl) IsScheduleRunning() bool {
+	return t.isScheduleRunning
+}
+
+func (t *taskImpl) IsAttached() bool {
+	return t.isAttached
+}
+
+func (t *taskImpl) Window() time.Duration {
+	return t.window
+}
+
+func (t *taskImpl) SetWindow(window time.Duration) {
+	t.window = window
+}
+
+func (t *taskImpl) NextEvent() time.Time {
+	return t.nextEvent
+}
+
+func (t *taskImpl) PrevEvent() time.Time {
+	return t.prevEvent
+}
+
+func (t *taskImpl) StartSchedule() {
+	// todo
+}
+
+func (t *taskImpl) StopSchedule() {
+	// todo
+}
+
+func (t *taskImpl) UpdateScheduleString(schedule string) error {
+	if schedule == t.schedule.OriginalText() {
+		return nil
+	}
+
+	sch, err := schyntax.New(schedule)
+	if err != nil {
+		return err
+	}
+
+	t.UpdateSchedule(sch)
+	return nil
+}
+
+func (t *taskImpl) UpdateSchedule(schedule schyntax.Schedule) {
+	// todo
 }
 
 func (t *taskImpl) runPendingEvent(ev *pendingEvent) {
-	//
+	// todo
 }
